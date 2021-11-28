9a554337bde0412e5888ec1846ec60f201b1fa56
Jerry <chenjingchun@foxmail.com>
2021-11-28 19:44:13 +0800

    删除不使用的分页方法

--- backend/Furion.Extras.Admin.NET/Util/XnPageResult.cs
+++ backend/Furion.Extras.Admin.NET/Util/XnPageResult.cs
@@ -11,36 +11,4 @@ namespace Furion.Extras.Admin.NET
         public int TotalRows { get; set; }
         public ICollection<T> Rows { get; set; }
     }
-
-    /// <summary>
-    /// 小诺分页列表结果
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public static class XnPageResult<T> where T : new()
-    {
-        public static dynamic PageResult(PagedList<T> page)
-        {
-            return new
-            {
-                PageNo = page.PageIndex,
-                PageSize = page.PageSize,
-                TotalPage = page.TotalPages,
-                TotalRows = page.TotalCount,
-                Rows = page.Items //.Adapt<List<T>>(),
-                //Rainbow = PageUtil.Rainbow(page.PageIndex, page.TotalPages, PageUtil.RAINBOW_NUM)
-            };
-        }
-
-        public static PageResult<V> PageResult<V>(PagedList<T> page)
-        {
-            return new PageResult<V>
-            {
-                PageNo = page.PageIndex,
-                PageSize = page.PageSize,
-                TotalPage = page.TotalPages,
-                TotalRows = page.TotalCount,
-                Rows = page.Items.Adapt<List<V>>(),
-            };
-        }
-    }
 }
